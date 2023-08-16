a837bb4ff14e08d7ecd4870c9e25ffea8b21ab51
Marko Nikolic <markonikolic770@gmail.com>
2023-08-16 21:40:36 +0200

    Testing new pattern

---
+++ app/src/main/java/com/example/movieviewer/data/models/LoggedInUser.kt
@@ -0,0 +1,6 @@
+package com.example.movieviewer.data.models
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(val userId: String, val displayName: String)
