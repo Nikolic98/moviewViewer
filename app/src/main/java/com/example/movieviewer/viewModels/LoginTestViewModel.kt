0eb2a4027fae3dcd1f113deda699cc91714de322
Marko Nikolic <markonikolic770@gmail.com>
2023-08-21 21:59:18 +0200

    cleanup

--- app/src/main/java/com/example/movieviewer/viewModels/LoginTestViewModel.kt
+++ app/src/main/java/com/example/movieviewer/viewModels/LoginTestViewModel.kt
@@ -3,7 +3,6 @@ package com.example.movieviewer.viewModels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import android.util.Patterns
 import com.example.movieviewer.R
 import com.example.movieviewer.data.LoginRepository
 import com.example.movieviewer.data.Result
